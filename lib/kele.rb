928c6e5f540dc0bd55b9c0ad9add0ae76b99e3ff
Corey <cdekk3r@hotmail.com>
2017-12-11 21:12:27 +0000

    get_me method to retrieve user

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'json'
 
 class Kele
    include HTTParty
@@ -6,9 +7,19 @@ class Kele
    base_uri 'https://www.bloc.io/api/v1'
    
    def initialize(email, password)
-      response = self.class.post('https://www.bloc.io/api/v1', body: {email: email, password: password})
+      response = self.class.post(api_url('sessions'), body: {email: email, password: password})
       @auth_token = response["auth_token"]
       raise "Invalid email or password. Try again." if @auth_token.nil?
    end
    
+   def get_me
+      response = self.class.get(api_url('users/me'), headers: { "authorization" => @auth_token })
+      @user = JSON.parse(response.body)
+   end
+   
+   private
+   
+   def api_url(endpoint)
+      "https://www.bloc.io/api/v1/#{endpoint}"
+   end
 end
